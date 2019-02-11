0d5cee47ef7c9a26f126e572a3c99859cbae62ed
David Soler Marco <dsoler@iti.es>
2019-02-11 17:11:57 +0100

    added xamarin and android studio

---
+++ Mqtt/Mqtt/MainActivity.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net.Mqtt;
+using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Mqtt.Services;
+using System.Collections.Generic;
+using Android.Content.PM;
+
+namespace Mqtt
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait)]
+    public class MainActivity : AppCompatActivity
+    {
+        private MqttService _mqttService;
+        private IList<string> _items;
+        private ArrayAdapter<string> _listAdapter;
+        ListView lista;
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            _mqttService = new MqttService();
+            lista = (ListView)FindViewById<ListView>(Resource.Id.listView);
+            _items = new List<string>();
+            _mqttService.MessageReceived += msg => RunOnUiThread(() =>
+            {
+                MqttServiceOnMessageReceived(msg);
+            });
+            _listAdapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, _items);
+            lista.Adapter = _listAdapter;
+        }
+
+        private void MqttServiceOnMessageReceived(MqttApplicationMessage mqttApplicationMessage)
+        {
+
+            // _listAdapter.NotifyDataSetChanged();
+
+            if (_listAdapter.Count < 10)
+            {
+                var msg = mqttApplicationMessage.Topic + ": " + System.Text.Encoding.UTF8.GetString(mqttApplicationMessage.Payload);
+                //_items.Add(mqttApplicationMessage.Topic + ": " + System.Text.Encoding.UTF8.GetString(mqttApplicationMessage.Payload));
+
+                //_listAdapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, _items);
+                //lista.Adapter = _listAdapter;
+                _listAdapter.Add(msg);
+                _listAdapter.NotifyDataSetChanged();
+
+            }
+
+
+        }
+    }
+}
---
+++ Mqtt/Mqtt/Services/MqttService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mqtt;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Mqtt.Services
+{
+    public class MqttService
+    {
+        private IMqttClient _mqttClient;
+        public event Action<MqttApplicationMessage> MessageReceived;
+
+        private void OnMessageReceived(MqttApplicationMessage msg)
+        {
+            MessageReceived?.Invoke(msg);
+        }
+        public MqttService()
+        {
+            Setup();
+        }
+
+        public async Task Setup()
+        {
+            _mqttClient = await MqttClient.CreateAsync("iot.eclipse.org", 1883);
+
+            await _mqttClient.ConnectAsync();
+            await _mqttClient.SubscribeAsync("#", MqttQualityOfService.AtMostOnce);
+            
+            _mqttClient
+                .MessageStream
+                .Subscribe(OnMessageReceived);
+        }
+    }
+}
